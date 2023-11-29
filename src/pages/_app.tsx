5c9eaa5b6c4459cf03b1f77c377009c63906d608
Samnang Lorn <lnang@cellcard.com.kh>
2023-11-29 16:21:31 +0700

    feat: apply nprogess

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,14 @@ import { MainLayout } from "@/layout/MainLayout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+import Router from "next/router";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
